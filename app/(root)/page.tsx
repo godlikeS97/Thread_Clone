318b93cfcac00e6a246c4ac29497c3dd63022eb2
godlikeS97 <958283294@qq.com>
2024-09-08 21:34:36 +0800

    finish HomePage(4 borders)

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,8 @@
-import { ClerkProvider, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>
-      <ClerkProvider>
-        <UserButton afterSignOutUrl="/" />
-      </ClerkProvider>
-    </div>
+    <>
+      <h1 className="head-text text-left">Home</h1>
+    </>
   );
 }
